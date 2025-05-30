02065829708944da0965e5c4bdba85142777f0d4
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-30 16:27:46 +0400

    segregated public and private routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,26 +4,39 @@ import { HomeComponent } from './features/home/home.component';
 import { ProductsComponent } from './features/products/products.component';
 import { BlogComponent } from './features/blog/blog.component';
 import { ProfileComponent } from './features/profile/profile.component';
+import { PublicComponent } from './layouts/public/public.component';
+import { PrivateComponent } from './layouts/private/private.component';
+import { privateGuard } from './guards/private.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'products',
-    component: ProductsComponent,
-  },
-  {
-    path: 'blog',
-    component: BlogComponent,
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
+    component: PublicComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'products',
+        component: ProductsComponent,
+      },
+      {
+        path: 'blog',
+        component: BlogComponent,
+      },
+    ],
   },
   {
     path: '',
+    component: PrivateComponent,
+    canActivateChild: [privateGuard],
+    children: [
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+    ],
   },
 ];
 
