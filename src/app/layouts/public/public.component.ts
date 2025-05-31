f6637893e1ecd3b1009296c06ed3454cc226ed75
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-31 21:10:26 +0400

    set up rudimentary nav for public pages

--- src/app/layouts/public/public.component.ts
+++ src/app/layouts/public/public.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Link } from 'src/app/models/link';
 
 @Component({
   selector: 'app-public',
   templateUrl: './public.component.html',
-  styleUrls: ['./public.component.css']
+  styleUrls: ['./public.component.css'],
 })
 export class PublicComponent {
-
+  links: Link[] = [
+    {
+      href: '/',
+      title: 'Home',
+    },
+    {
+      href: '/products',
+      title: 'Products',
+    },
+    {
+      href: '/blog',
+      title: 'Blog',
+    },
+    {
+      href: '/profile',
+      title: 'Profile',
+    },
+  ];
 }
