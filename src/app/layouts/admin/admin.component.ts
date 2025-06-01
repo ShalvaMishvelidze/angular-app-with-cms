e767b6cfcf410e9c6f1294987265f5da886bcfb0
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-06-01 12:56:17 +0400

    set up navigation and routing for all pages

--- src/app/layouts/admin/admin.component.ts
+++ src/app/layouts/admin/admin.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent {
-
+  links = [
+    {
+      href: '/users',
+      title: 'Users',
+    },
+    {
+      href: '/products',
+      title: 'Products',
+    },
+    {
+      href: '/posts',
+      title: 'Posts',
+    },
+    {
+      href: '/orders',
+      title: 'Orders',
+    },
+  ];
 }
--- src/app/layouts/private/private.component.ts
+++ src/app/layouts/private/private.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Link } from 'src/app/models/link';
 
 @Component({
   selector: 'app-private',
   templateUrl: './private.component.html',
-  styleUrls: ['./private.component.css']
+  styleUrls: ['./private.component.css'],
 })
 export class PrivateComponent {
-
+  links: Link[] = [
+    {
+      href: '/my-products',
+      title: 'My Products',
+    },
+    {
+      href: '/my-posts',
+      title: 'My Posts',
+    },
+    {
+      href: '/orders',
+      title: 'Order History',
+    },
+  ];
 }
