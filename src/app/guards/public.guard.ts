2ed0191371c697317c9900cc708a53df4910d3fb
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-06-02 16:46:09 +0400

    bug: no public guard

--- src/app/guards/public.guard.ts
+++ src/app/guards/public.guard.ts
@@ -1,22 +1,10 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { toObservable } from '@angular/core/rxjs-interop';
 
 export const publicGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const authService = inject(AuthService);
-
-  const user = toObservable(authService.user);
-
-  user.subscribe((u) => {
-    if (u) {
-      console.warn('Authentication is still pending');
-    } else {
-      console.log('Authentication check completed');
-      router.navigate(['/']);
-    }
-  });
 
   return true;
 };
