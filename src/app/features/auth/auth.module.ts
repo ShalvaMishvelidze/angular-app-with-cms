1bfe0198fc4b804e2f6f19efb1b0b9c5035b8a93
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-06-03 11:41:10 +0400

    added register/login links to auth pages for ease of access

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, AppRoutingModule],
 })
 export class AuthModule {}
