0a918c940fec88b66f49ce5cbaff4ce9121576bd
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-06-26 13:48:50 +0400

    implemented displaying cart total for the user in the nav bar

--- src/app/components/user-button/user-button.component.ts
+++ src/app/components/user-button/user-button.component.ts
@@ -1,28 +1,33 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
-    selector: 'app-user-button',
-    templateUrl: './user-button.component.html',
-    styleUrls: ['./user-button.component.css'],
-    standalone: false
+  selector: 'app-user-button',
+  templateUrl: './user-button.component.html',
+  styleUrls: ['./user-button.component.css'],
+  standalone: false,
 })
 export class UserButtonComponent {
   private authService = inject(AuthService);
+  private cartService = inject(CartService);
   readonly _dropdown = signal<boolean>(false);
-  initials: string | null = null;
 
-  constructor() {
-    effect(() => {
-      const user = this.authService.user();
-      this.initials =
-        user && user.name && user.lastName
-          ? user.name[0] + user.lastName[0]
-          : null;
-    });
-  }
+  initials = computed(() => {
+    const user = this.authService.user();
+    return user && user.name && user.lastName
+      ? user.name[0] + user.lastName[0]
+      : '';
+  });
+  total = computed(() => this.cartService.cartItems().totalItems);
 
   toggleDropdown() {
     this._dropdown.set(!this._dropdown());
+  }
+
+  constructor() {
+    effect(() => {
+      this.cartService.getCartItems();
+    });
   }
 }
